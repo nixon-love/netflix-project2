e5a0f688505a04fc78598d5be53a589e1248e430
nixon abanda <nixonkinge@gmail.com>
2022-09-19 12:09:09 -0400

    prod operation19

---
+++ rds.tf
@@ -0,0 +1,3 @@
+# creating an amazon rds codegit
+# amazon aurora mx.5large
+# ebs volume and snapshots@
---
+++ s3bucket.tf
@@ -0,0 +1,3 @@
+# us-east-1 bucket for netflix videos
+# os windows
+# bucket must be public
